7cd5518984ce43f8d290c19d8d172c327dd2f457
Patrick Keroulas <patrick.keroulas@radio-canada.ca>
2022-08-12 10:44:09 -0400

    improve IP allocation

--- setup.sh
+++ setup.sh
@@ -3,20 +3,30 @@
 echo "---------------------------------------------"
 echo "           Populate conf with"
 echo "---------------------------------------------"
-cat .env
 source .env
+
+SUBNET=${SUBNET_PREFIX}.0
+GATEWAY_IP=${SUBNET_PREFIX}.1
+DHCP_IP=${SUBNET_PREFIX}.2
+TEST_TOOL_IP=${SUBNET_PREFIX}.3
+
+echo "Interface:          $IFACE"
+echo "Subnet:             $SUBNET"
+echo "Gateway:            $GATEWAY_IP"
+echo "Nmos-testing + DNS: $TEST_TOOL_IP"
+echo "Dhcp server:        $DHCP_IP"
 
 echo "---------------------------------------------"
 echo "           dhcpd:"
 echo "---------------------------------------------"
 sed -i "\
 s,\(^option domain-name-servers\).*;,\1 $TEST_TOOL_IP;,;
-s,\(^subnet\) .* \(netmask 255.255.255.0 {\),\1 $SUBNET_PREFIX.0 \2,;
+s,\(^subnet\) .* \(netmask 255.255.255.0 {\),\1 $SUBNET \2,;
 s,\(    option broadcast-address\).*;,\1 $SUBNET_PREFIX.255;,;
 s,\(    option routers\).*;,\1 $GATEWAY_IP;,;
-s,\(    range\).*;,\1 $SUBNET_PREFIX.10 $SUBNET_PREFIX.99;,
+s,\(    range\).*;,\1 $SUBNET_PREFIX.5 $SUBNET_PREFIX.99;,
 " ./dhcp/dhcpd.conf
 cat ./dhcp/dhcpd.conf
 
 echo "---------------------------------------------"
-sed -i "s,\(CONFIG.DNS_UPSTREAM_IP = \).*,\1 '$GATEWAY_IP'," ./UserConfig.py
+sed -i "s,\(CONFIG.DNS_UPSTREAM_IP =\).*,\1 '$GATEWAY_IP'," ./UserConfig.py
